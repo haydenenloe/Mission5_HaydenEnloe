c6c84f7e6e34ba7118e9511166c8184bea16464e
haydenenloe <haydenenloe@HAYDENENLOE26B5>
2024-02-08 10:26:24 -0700

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $("#calculateButton").click(function () {
+        let hours = parseInt($("#hours").val());
+
+        if (isNaN(hours) || hours < 0) {
+            alert("Please enter a valid positive number for hours.");
+            return;
+        }
+
+        let hourlyRate = parseInt($("#hourlyRate").val().substring(1));
+        let total = hours * hourlyRate;
+        $("#total").text("$" + total);
+    });
+});
